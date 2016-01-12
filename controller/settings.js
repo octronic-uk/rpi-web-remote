f359f88183afdbcb768df56c658edcb41f353193
Ashley Thompson <ashthompson06@gmail.com>
2016-01-12 09:40:28 +0000

    Work on Settings Page

--- controller/settings.js
+++ controller/settings.js
@@ -3,5 +3,17 @@ PiApp.controller('Settings',
 	function($state, $stateParams, $controller, $cookies, $http, $scope, $rootScope)
 	{
     $controller('PiApp', {$scope: $scope});
+		$scope.serialPortList = [];
+		$scope.serialPortNames = [];
+
+		$scope.getDeviceSerialPortsApi(function(serialList)
+		{
+			$scope.serialPortList = serialList;
+			$scope.serialPortNames = [];
+			$scope.serialPortList.forEach(function(sp)
+			{
+				$scope.serialPrtNames.push(sp.comName);
+			});
+		});
   }
 ]);
