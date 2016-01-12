612d5bbfebddaaaa1d521e9c643e8255f2a01818
Ashley Thompson <ashthompson06@gmail.com>
2016-01-12 11:18:09 +0000

    Added setting update alert

--- controller/settings.js
+++ controller/settings.js
@@ -1,43 +1,56 @@
 PiApp.controller('Settings',
-	['$state','$stateParams','$controller','$cookies','$http','$scope','$rootScope' ,
-	function($state, $stateParams, $controller, $cookies, $http, $scope, $rootScope)
+['$state','$stateParams','$controller','$cookies','$http','$scope','$rootScope' ,
+function($state, $stateParams, $controller, $cookies, $http, $scope, $rootScope)
+{
+	$controller('PiApp', {$scope: $scope});
+	$scope.serialPortList = [];
+
+	$scope.alerts = [];
+
+	$scope.addAlert = function(alert)
 	{
-    $controller('PiApp', {$scope: $scope});
-		$scope.serialPortList = [];
+		$scope.alerts.push(alert);
+	};
 
-		$scope.getDeviceSerialListApi(function(serialList)
-		{
-			$scope.serialPortList = serialList;
-		});
+	$scope.closeAlert = function(index)
+	{
+		$scope.alerts.splice(index, 1);
+	};
 
-		$scope.getDeviceSerialBaudrateList(function(baudList)
-		{
-			$scope.baudRateList = baudList;
-		});
+	$scope.getDeviceSerialListApi(function(serialList)
+	{
+		$scope.serialPortList = serialList;
+	});
 
-		$scope.saveSerialSettingsButton = function()
+	$scope.getDeviceSerialBaudrateList(function(baudList)
+	{
+		$scope.baudRateList = baudList;
+	});
+
+	$scope.saveSerialSettings = function()
+	{
+		$scope.setDeviceSerialPathApi($scope.selectedSerialPort,function(result)
 		{
-			$scope.setDeviceSerialPathApi($scope.selectedSerialPort,function(result)
+			if (result)
 			{
-				if (result)
+				$scope.setDeviceSerialBaudrateApi($scope.selectedBaudrate,function(result)
 				{
-						$scope.setDeviceSerialBaudrateApi($scope.selectedBaudrate,function(result)
-						{
-							if (result)
-							{
-								console.log("Settings saved successfuly");
-							}
-							else
-							{
-								console.log("Error setting serial device baudrate");
-							}
-						});
-				}
-				else
-				{
-					consoe.log("Error setting serial device path");
-				}
-			});
-		}
-  }
+					if (result)
+					{
+						console.log("Settings saved successfuly");
+						addAlert({ type: 'success', msg: 'Serial settings have been saved!.' });
+					}
+					else
+					{
+						console.log("Error setting serial device baudrate");
+					}
+				});
+			}
+			else
+			{
+				consoe.log("Error setting serial device path");
+			}
+		});
+	}
+}
 ]);
