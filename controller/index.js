205faf4d1a929601d87b24e43184447ccccf49dd
Ash Thompson <ashthompson06@gmail.com>
2016-01-25 23:03:49 +0000

    Ammend last

---
+++ controller/index.js
@@ -0,0 +1,20 @@
+
+/*
+  Ash's RaspberryPI IO Remote.
+  email: ashthompson06@gmail.command
+  repo: https://github.com/BashEdThomps/IoT-RaspberryPI.git
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+App.controller('Index',['appApi','$scope',function(appApi, $scope) {}]);
