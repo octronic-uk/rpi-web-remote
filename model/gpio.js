b6bb46b9ca15ddae6550dba6fb47900c95fb5c6e
Ash Thompson <ashthompson06@gmail.com>
2016-01-28 09:08:19 +0000

    sigh

--- model/gpio.js
+++ model/gpio.js
@@ -203,6 +203,8 @@ var putPinState = function(request,response) {
         if (err) {
           util.sendHttpError(response,"Unable to set output of pin (gpio.write error) "+pinId+" "+err);
         } else {
+          pin.state = val;
+          _addPinEvent(pin);
           util.sendHttpOK(response);
         }
       });
